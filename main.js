866ef35a3bcd28a5733a7f6bc3b37eeeff8fc401
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-05-23 23:05:02 +0500

    bug fixed

---
+++ main.js
@@ -0,0 +1,40 @@
+var canvas = document.getElementById('myCanvas');
+var cntx = canvas.getContext('2d');
+var painting = document.getElementById('content');
+var paintStyle = getComputedStyle(painting);
+canvas.width = parseInt(paintStyle.getPropertyValue('width'));
+canvas.height = parseInt(paintStyle.getPropertyValue('height'));
+
+var mouse = { x: 0, y: 0 };
+
+canvas.addEventListener('mousemove', function (e) {
+    mouse.x = e.pageX - this.offsetLeft;
+    mouse.y = e.pageY - this.offsetTop;
+}, false);
+
+cntx.lineWidth = 5
+cntx.lineJoin = 'round';
+cntx.lineCap = 'round';
+
+function changethickness() {
+    thickness = document.getElementById('thickness').value;
+    cntx.lineWidth = thickness;
+}
+function changecolor() {
+    color = document.getElementById('color').value;
+    cntx.strokeStyle = color;
+}
+
+canvas.addEventListener('mousedown', function (e) {
+    cntx.beginPath();
+    cntx.moveTo(mouse.x, mouse.y);
+    canvas.addEventListener('mousemove', onPaint, false);
+}, false);
+canvas.addEventListener('mouseup', function () {
+    canvas.removeEventListener('mousemove', onPaint, false);
+}, false);
+
+var onPaint = function () {
+    cntx.lineTo(mouse.x, mouse.y);
+    cntx.stroke();
+};
